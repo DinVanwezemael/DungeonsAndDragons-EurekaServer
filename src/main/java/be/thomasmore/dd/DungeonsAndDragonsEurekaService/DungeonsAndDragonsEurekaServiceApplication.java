944f1da46b552c40d0a30d07ea8812537863f1df
mnmpower <mnmmaarten@hotmail.com>
2019-11-15 09:20:18 +0100

    working for raceservice

--- src/main/java/be/thomasmore/dd/DungeonsAndDragonsEurekaService/DungeonsAndDragonsEurekaServiceApplication.java
+++ src/main/java/be/thomasmore/dd/DungeonsAndDragonsEurekaService/DungeonsAndDragonsEurekaServiceApplication.java
@@ -11,5 +11,6 @@ public class DungeonsAndDragonsEurekaServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DungeonsAndDragonsEurekaServiceApplication.class, args);
 	}
-
 }
+
+
